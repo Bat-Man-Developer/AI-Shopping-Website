e64d1d6d827e54cbc47b5878f90acb126d0819de
kay mudau <kkay.mudau008@gmail.com>
2024-05-29 22:35:41 +0200

    commit

--- layouts/header.php
+++ layouts/header.php
@@ -27,14 +27,16 @@ session_start();
 			    <nav>
 						<ul id="menuitems">
 							<li class="exitmenutogglebtn" id="nav-exit" onclick="menutoggle()"><a href="#">X</a></li>
-							<li class="active"><a href="index.php"><img id="navbaricons" src="assets/images/bx-home.svg" alt="Snow">Home</a></li>
-							<li class="active"><a href="about.php"><img id="navbaricons" src="assets/images/bx-info-square.svg" alt="Snow">About</a></li>
-							<li class="active" id="departmentitems" onclick="departmentsmenutoggle()"><a href="#"><img id="navbaricons" src="assets/images/bx-store-alt.svg" alt="Snow">Shop Departments</a></li>
-							<li class="active"><a href="contact.php"><img id="navbaricons" src="assets/images/bx-phone.svg" alt="Snow">Contact</a></li>
-							<li class="active"><a href="sellers/sellerslogin.php" target="_blank" rel="noopener noreferrer"><img id="navbaricons" src="assets/images/bx-network-chart.svg" alt="Snow">Sell on NSSA</a></li>
-							<li class="active"><a href="#"><img id="navbaricons" src="assets/images/bx-buildings.svg" alt="Snow">Careers</a></li>
+							<li class="active"><a href="index.php"><img id="navbaricons" src="assets/images/bx-home.svg" alt="Snow" style="display: none">Home</a></li>
+							<li class="active"><a href="about.php"><img id="navbaricons" src="assets/images/bx-info-square.svg" alt="Snow" style="display: none">About</a></li>
+							<li class="active" id="departmentitems" onclick="departmentsmenutoggle()"><a href="#"><img id="navbaricons" src="assets/images/bx-store-alt.svg" alt="Snow" style="display: none">Shop Departments</a></li>
+							<li class="active"><a href="contact.php"><img id="navbaricons" src="assets/images/bx-phone.svg" alt="Snow" style="display: none">Contact</a></li>
+							<li class="active"><a href="sellers/sellerslogin.php" target="_blank" rel="noopener noreferrer"><img id="navbaricons" src="assets/images/bx-network-chart.svg" alt="Snow" style="display: none">Sell on NSSA</a></li>
+							<li class="active"><a href="#"><img id="navbaricons" src="assets/images/bx-buildings.svg" alt="Snow" style="display: none">Careers</a></li>
 				    </ul>
 					</nav>
+					<!---------------Wishlist Image---------------->
+					<a href="wishlist.php" style="margin-right: 3%;"><img id="wishlistpic" class="wishlistpic" src="assets/images/wishlist_pic.png" alt="Snow" width="30px" height="32px" align="left"></a>
 					<!---------------Account Image---------------->
 					<a href="login.php" style="margin-right: 3%;"><img id="accountpic" class="accountpic" src="assets/images/accounticon_pic.png" alt="Snow" width="30px" height="32px" align="left"></a>
 					<!---------------Cart Image---------------->
@@ -44,6 +46,16 @@ session_start();
 					<?php } ?></a>
 					<!-- Menu icon -->
 					<img src="assets/images/menu.png" alt="Snow" class="menu-icon" onclick="menutoggle()" align="center">
+				</div>
+				<!---Search Bar--->
+				<div class="searchProductsContainer">
+					<form name="searchProductsForm" method="POST" action="products.php">
+						<div class="searchProductsDiv">
+								<input type="text" id="searchInput" name="searchproductstring" placeholder="Search...">
+								<button type="submit" id="searchButton">Search</button>
+								<div id="suggestionsContainer"></div>
+						</div>
+					</form>
 				</div>
 				<!-------- Departments Navbar ------->
 				<div class="departmentsnavbar" id="departmentsnavbar">
@@ -89,15 +101,4 @@ session_start();
 				<!------ Js for Voice Recognition Output ----->
 				<script src="js/getvoicerecognitionoutput.js"></script>
 			</div>
-		</div>
-		
-		<!---Search Bar--->
-		<div class="searchProductsContainer">
-			<form name="searchProductsForm" method="POST" action="products.php">
-				<div class="searchProductsDiv">
-						<input type="text" id="searchInput" name="searchproductstring" placeholder="Search...">
-						<button type="submit" id="searchButton">Search</button>
-						<div id="suggestionsContainer"></div>
-				</div>
-			</form>
 		</div>
