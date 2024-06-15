6c150adeada2da8ab15b135298183aaa9ad18979
kay mudau <kkay.mudau008@gmail.com>
2024-06-15 14:10:56 +0200

    commit

--- cart.php
+++ cart.php
@@ -18,8 +18,6 @@ if(empty($_SESSION['cart'])){
 			<link rel="stylesheet" type="text/css" href="assets/styles/stylecart.css">
 			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
     	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		</head>
 	  <body>
