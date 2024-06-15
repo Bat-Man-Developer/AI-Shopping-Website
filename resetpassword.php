af434bc973acc22512f2c7032f1b69b9ccbd10aa
kay mudau <kkay.mudau008@gmail.com>
2024-06-15 13:32:07 +0200

    commit

--- resetpassword.php
+++ resetpassword.php
@@ -11,7 +11,6 @@ include('server/getresetpassword.php');
 			<link rel="stylesheet" type="text/css" href="assets/styles/styledefault.css">
 			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
     	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
