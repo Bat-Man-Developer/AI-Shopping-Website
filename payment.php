42ae96e55783b420e1c7efa6bc9f4a980565f740
kay mudau <kkay.mudau008@gmail.com>
2024-06-15 13:23:04 +0200

    commit

--- payment.php
+++ payment.php
@@ -46,9 +46,9 @@ include('server/getpayment.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NSSA Payment</title>
     <link rel="stylesheet" href="assets/styles/stylepayment.css">
-    <link rel="stylesheet" type="text/css" href="assets/styles/all.css">
-		<link rel="stylesheet" type="text/css" href="assets/styles/fontawesome.css">
-    <link rel="stylesheet" type="text/css" href="assets/styles/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
